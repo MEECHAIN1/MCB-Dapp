b018e38195a65b53ed4247dd5b6f3516daa7b1e6
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2025-12-31 08:33:46 +0700

    Updated contracts.ts

--- lib/contracts.ts
+++ lib/contracts.ts
@@ -23,7 +23,7 @@ export const ADRS = {
   nft: getEnv("VITE_NFT_ADDRESS", "0x5FbDB2315678afecb367f032d93F642f64180aa3") as `0x${string}`,
   marketplace: getEnv("VITE_SWAP_ADDRESS", "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9") as `0x${string}`,
   staking: getEnv("VITE_STAKING_ADDRESS", "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512") as `0x${string}`,
-  token: getEnv("VITE_TOKEN_ADDRESS", "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512") as `0x${string}`, 
+  token: getEnv("VITE_TOKEN_ADDRESS", "0x8Da6Eb1cd5c0C8cf84bD522AB7c11747DB1128C9") as `0x${string}`, 
   miner: getEnv("VITE_MINER_ADDRESS", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8") as `0x${string}`,
 };
 
