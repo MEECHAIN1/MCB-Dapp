e36ca2696a05d7fdfa0ccc81189158840d986a45
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2025-12-31 15:03:03 +0700

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { createConfig, http, WagmiProvider, useAccount, useChainId } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from 'tanstack/react-query';
 import { localhost } from 'viem/chains';
 import { StatusOverlay } from './components/StatusOverlay';
 import { Navbar } from './components/Navbar';
