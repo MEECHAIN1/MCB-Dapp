cc929eba37913d69bc9b6014f79a5b4e31588085
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2025-12-31 16:14:12 +0700

    Updated Environment.tsx

--- Environment.tsx
+++ Environment.tsx
@@ -5,7 +5,7 @@ export function MeeBotScene() {
     <>
       {/* ระบบจะวิ่งไปหาที่ public/hdri/ โดยอัตโนมัติเมื่อรันบนเบราว์เซอร์ */}
       <Environment 
-        files="/hdri/dikhololo_night_1k.hdr" 
+        files="/hdri/dikhololo_night_1k.hdr",
         background 
         blur={0.05} // เพิ่มความฟุ้งของพื้นหลังเล็กน้อยเพื่อให้หุ่นยนต์ดูเด่นขึ้น
       />
