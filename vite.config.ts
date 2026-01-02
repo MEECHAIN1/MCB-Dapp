c71f5e0da0f6b64115c1d9a65ffb90e2cb26c2e0
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-03 03:10:32 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
       allowedHosts: true,
     },
     define: {
-      'import.meta.env.VIET_API_KEY': JSON.stringify(env.API_KEY || ""),
+      'import.meta.env.VIET_API_KEY': JSON.stringify(env.VITE_API_KEY || ""),
       'global': 'globalThis',
     }
   };
